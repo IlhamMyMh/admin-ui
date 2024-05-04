88bcdfc401b7c4a8a3d4b1c7ed6b9a290fe85072
IlhamMyMh <husnan.muzakki.r1c@gmail.com>
2024-05-04 22:37:42 +0700

    ubah teks di app 3

--- src/pages/single/Single.jsx
+++ src/pages/single/Single.jsx
@@ -1,9 +1,60 @@
 import "./single.scss";
+import Sidebar from "../../components/sidebar/Sidebar";
+import Navbar from "../../components/navbar/Navbar";
+import Chart from "../../components/chart/Chart";
+import List from "../../components/datatable/Datatable";
 
 const Single = () => {
-    return(
-        <div>Single</div>
-    )
+  return (
+    <div className="single">
+      <Sidebar />
+      <div className="singleContainer">
+        <Navbar />
+        <div className="top">
+        <div className="left">
+  <div className="editButton">Edit</div>
+  <h1 className="title">Information</h1>
+  <div className="item">
+    <img
+      src="https://static.miraheze.org/bluearchivewiki/c/c2/Hoshino_%28Swimsuit%29.png"
+      alt=""
+      className="itemImg"
+    />
+    <div className="details">
+      <h1 className="itemTitle">Takanashi Hoshino</h1>
+      <div className="detailItem">
+        <span className="itemKey">Email:</span>
+        <span className="itemValue">Ojiisan@gmail.com</span>
+      </div>
+      <div className="detailItem">
+        <span className="itemKey">Phone:</span>
+        <span className="itemValue">+1 2345 67 89</span>
+      </div>
+      <div className="detailItem">
+        <span className="itemKey">Address:</span>
+        <span className="itemValue">
+          Kivotos St. Abydos Yd. Tokyo
+        </span>
+      </div>
+      <div className="detailItem">
+        <span className="itemKey">Country:</span>
+        <span className="itemValue">JAPAN</span>
+      </div>
+    </div>
+  </div>
+</div>
+<div className="right">
+  <Chart aspect={3 / 1} type="order" />
+</div>
+</div>
+<div className="bottom">
+  <h1 className="title">Last Transactions</h1>
+  <List />
+</div>
+</div>
+</div>
+
+  );
 };
 
-export default Single
+export default Single;
