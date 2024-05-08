d932262430b4374a60e91c808140017269406a85
IlhamMyMh <husnan.muzakki.r1c@gmail.com>
2024-05-08 21:19:12 +0700

    ubah teks di app 5

--- src/components/datatable/Datatable.jsx
+++ src/components/datatable/Datatable.jsx
@@ -72,6 +72,7 @@ const Datatable = () => {
         checkboxSelection
       />
     </div>
+    
   );
 };
 
--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -6,6 +6,7 @@ import StorefrontIcon from '@mui/icons-material/Storefront';
 import LocalGroceryStoreIcon from '@mui/icons-material/LocalGroceryStore';
 import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
 import LogoutIcon from '@mui/icons-material/Logout';
+import CategoryIcon from '@mui/icons-material/Category';
 
 const Sidebar = () => {
   return (
@@ -40,6 +41,12 @@ const Sidebar = () => {
             <LocalGroceryStoreIcon className="icon"/>
             <span>Orders</span>
         </li>
+        <Link to="/categories">
+        <li>
+          <CategoryIcon className="icon"/>
+          <span>Categories</span>
+        </li>
+        </Link>
         <p className="title">USER</p>
         <li>
             <AccountCircleOutlinedIcon className="icon"/>
