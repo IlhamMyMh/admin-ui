7f7723749b650342ff44e19afd48b1482e13a345
IlhamMyMh <husnan.muzakki.r1c@gmail.com>
2024-05-26 03:54:05 +0700

    ubah teks di app 7

--- src/pages/single/Single.jsx
+++ src/pages/single/Single.jsx
@@ -11,38 +11,38 @@ const Single = () => {
       <div className="singleContainer">
         <Navbar />
         <div className="top">
-        <div className="left">
-  <div className="editButton">Edit</div>
-  <h1 className="title">Information</h1>
-  <div className="item">
-    <img
-      src="https://static.miraheze.org/bluearchivewiki/c/c2/Hoshino_%28Swimsuit%29.png"
-      alt=""
-      className="itemImg"
-    />
-    <div className="details">
-      <h1 className="itemTitle">Takanashi Hoshino</h1>
-      <div className="detailItem">
-        <span className="itemKey">Email:</span>
-        <span className="itemValue">Ojiisan@gmail.com</span>
-      </div>
-      <div className="detailItem">
-        <span className="itemKey">Phone:</span>
-        <span className="itemValue">+1 2345 67 89</span>
-      </div>
-      <div className="detailItem">
-        <span className="itemKey">Address:</span>
-        <span className="itemValue">
-          Kivotos St. Abydos Yd. Tokyo
-        </span>
-      </div>
-      <div className="detailItem">
-        <span className="itemKey">Country:</span>
-        <span className="itemValue">JAPAN</span>
-      </div>
-    </div>
-  </div>
-</div>
+            <div className="left">
+            <div className="editButton">Edit</div>
+            <h1 className="title">Information</h1>
+            <div className="item">
+              <img
+                src="https://images.pexels.com/photos/733872/pexels-photo-733872.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260"
+                alt=""
+                className="itemImg"
+              />
+              <div className="details">
+                <h1 className="itemTitle">Jane Doe</h1>
+                <div className="detailItem">
+                  <span className="itemKey">Email:</span>
+                  <span className="itemValue">janedoe@gmail.com</span>
+                </div>
+                <div className="detailItem">
+                  <span className="itemKey">Phone:</span>
+                  <span className="itemValue">+1 2345 67 89</span>
+                </div>
+                <div className="detailItem">
+                  <span className="itemKey">Address:</span>
+                  <span className="itemValue">
+                    Elton St. 234 Garden Yd. NewYork
+                  </span>
+                </div>
+                <div className="detailItem">
+                  <span className="itemKey">Country:</span>
+                  <span className="itemValue">USA</span>
+                </div>
+              </div>
+            </div>
+          </div>
           <div className="right">
             <Chart aspect={3 / 1} type="order" />
           </div>
